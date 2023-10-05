730318008588a0b8d891885681a48c32badc65e2
Sid <roysid2002@gmail.com>
2023-10-06 00:07:33 +0530

    Complete layout sorted including buttons and positioning of the ButtonBox

--- src/App.js
+++ src/App.js
@@ -4,18 +4,35 @@ import Screen from './components/Screen';
 import ButtonBox from './ButtonBox';
 import Button from './Button';
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="]
+];
+
 function App() {
   return (
     <div className="container">
       <Body>
-      <Screen value={"0"}></Screen>
-      <ButtonBox>
-        <Button 
-        className=""
-        value="0"
-        onClick={() => {console.log("Button clicked!");}}
-        ></Button>
-      </ButtonBox>
+        <Screen value="0"></Screen>
+        <ButtonBox>
+          {
+            btnValues.flat().map((btn, i) => {
+              return (
+                <Button
+                  key={i}
+                  className={btn === "=" ? "equals" : ""}
+                  value={btn}
+                  onClick={() => {
+                    console.log(`${btn} clicked!`);
+                  }}
+                />
+              )
+            })
+          }
+        </ButtonBox>
       </Body>
     </div>
   );
