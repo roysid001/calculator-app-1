b8637396c145cc706dcbf1fa75408700b4cab709
Sid <roysid2002@gmail.com>
2023-10-09 01:28:30 +0530

    percentClickHandler function defined

--- src/App.js
+++ src/App.js
@@ -81,6 +81,17 @@ function App() {
     }
   }
 
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0;
+    let res = calc.res ? parseFloat(calc.res) : 0;
+    setCalc({
+      ...calc,
+      num: (num / Math.pow(100, 1)),
+      res: (res / Math.pow(100, 1)),
+      sign: ""
+    });
+  }
+
   return (
     <div className="container">
       <Body>
